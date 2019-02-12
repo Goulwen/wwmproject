13291e123dab807c1a5fee9aadfae07b12a3fcfb
goulwen <goulwen3@hotmail.com>
2019-02-12 17:01:53 +0100

    instruction

---
+++ app/src/main/java/com/bgeiotdev/eval/InstructionsActivity.java
@@ -0,0 +1,17 @@
+package com.bgeiotdev.eval;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class InstructionsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+
+    public void onGoBackButtonClicked(View view) {
+        finish();
+    }
+}
